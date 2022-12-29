7cf7c7fbaefab4646b7b6457bc508f0ac1455446
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-12-28 23:48:15 -0800

    debug edge

--- pages/api/img/[...url].js
+++ pages/api/img/[...url].js
@@ -153,7 +153,7 @@ export default async function handler(req) {
       height: 630,
     })
   } catch (e) {
-    return new Response(`Failed to generate image`, {
+    return new Response(`Failed to generate image: ${e}`, {
       status: 500,
     })
   }
