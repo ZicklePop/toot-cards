e935d5378c8b7747a7d175493e1500abce93d6e3
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-12-29 00:30:08 -0800

    status api moved to edge

--- pages/api/status/[...url].js
+++ pages/api/status/[...url].js
@@ -1,6 +1,19 @@
 import getStatusFromParams from '../../../lib/get-status-from-params'
 
-export default async function response({ query: { url } }, res) {
-  const json = await getStatusFromParams(url)
-  res.status(200).json({ ...json })
+export const config = {
+  runtime: 'edge',
+}
+
+function cleanUrl(url = '') {
+  return url.pathname.replace('/api/status/', '')
+}
+
+export default async function handler(req) {
+  const url = cleanUrl(req.nextUrl)
+  const json = await getStatusFromParams(url.split('/'))
+  return new Response(JSON.stringify(json), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 }
