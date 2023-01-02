36273a2995c5ed920f9c4ca207fb46e050af8a14
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-01-01 22:10:55 -0800

    nit

--- ui/user-details.js
+++ ui/user-details.js
@@ -1,14 +1,17 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
 export default function UserDetails({ avatar, display_name, url, username }) {
   const host = url?.split('/')[2]
   return (
     <div className="pb-4">
       <a href={url} className="flex cursor-pointer flex-row items-center">
         <div className="shrink-0 pr-3">
-          <img
+          <Image
             src={avatar}
             alt={`${display_name}'s avatar`}
             className="aspect-square h-16 w-16 rounded-md"
+            width={128}
+            height={128}
           />
         </div>
         <div className="overflow-hidden break-words">
