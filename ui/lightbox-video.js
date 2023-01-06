15e3d577d325a2567140d2ab10e69587d833e650
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-01-05 18:14:34 -0800

    turning off autoplay on videos for now

--- ui/lightbox-video.js
+++ ui/lightbox-video.js
@@ -2,7 +2,6 @@ export default function LightboxVideo({ id, description, preview_url, url }) {
   return (
     <video
       aria-label={description || 'Video attachment'}
-      autoPlay
       className="max-h-[100svh] object-contain drop-shadow-2xl"
       controls
       id={id}
