e004635aa9d0b7226e5f3af4016930e6e844dfe8
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-01-25 20:21:44 -0800

    i was not impressed

--- ui/status-time.js
+++ ui/status-time.js
@@ -1,13 +1,21 @@
+'use client'
+
 import formatDate from 'lib/format-date'
+import { useState, useEffect } from 'react'
 
 export default function StatusTime({ time, ...rest }) {
+  const [showLocalTime, setShowLocalTime] = useState(false)
+  useEffect(() => {
+    setShowLocalTime(true)
+  }, [])
+
   return (
     <>
       <time className="hidden md:block" dateTime={time} {...rest}>
-        {formatDate(time)}
+        {showLocalTime && formatDate(time)}
       </time>
       <time className="block md:hidden" dateTime={time} {...rest}>
-        {formatDate(time, true)}
+        {showLocalTime && formatDate(time, true)}
       </time>
     </>
   )
