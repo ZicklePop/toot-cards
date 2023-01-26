eafef562dd4d780a0a51e340deed78c99d0c4eba
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-01-25 20:38:29 -0800

    design nit

--- ui/status-box.js
+++ ui/status-box.js
@@ -1,6 +1,6 @@
 export default function StatusBox({ children, ...rest }) {
   return (
-    <div className="flex pr-3 md:pr-0 md:pl-3" {...rest}>
+    <div className="flex items-start pr-3 md:pr-0 md:pl-3" {...rest}>
       {children}
     </div>
   )
