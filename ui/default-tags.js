3db2868d6f68a94e507e3c06b5aa160e2e006dd5
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2023-08-07 17:44:30 -0700

    Update default-tags.js

--- ui/default-tags.js
+++ ui/default-tags.js
@@ -6,6 +6,7 @@ export default function DefaultTags() {
       <meta charSet="utf-8" />
       <meta name="keywords" content="mastodon, toot, post, status" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="robots" content="noai" />
     </>
   )
 }
